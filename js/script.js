e9c825036f1f2c3b05407deeeef219e370ffa87f
Lera Smirnova <smirvaler89@gmail.com>
2018-05-05 13:58:04 +0300

    added 2mod

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let userInput;
+            const numbers = [];
+            let total = 0;
+
+            for (let i = 0; i < Infinity; i++) {
+                userInput = prompt('введите число');
+                if (userInput === null) break;
+                numbers.push(+userInput);
+                total = total + parseInt(userInput);
+            }
+            console.log(numbers);
+            alert(`Сумма чисел равна ${total}`);
