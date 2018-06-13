b48d21b4d8a49c235da46f54be71197c886af51d
Lera Smirnova <smirvaler89@gmail.com>
2018-06-13 15:09:13 +0300

    fix 3mod

--- mod-2/js/script.js
+++ mod-2/js/script.js
@@ -3,13 +3,13 @@ const numbers = [];
 let total = 0;
 
 for (let i = 0; i < Infinity; i++) {
-    userInput = prompt('введите число');
+    // userInput = prompt('введите число');
     if (userInput === null) break;
     numbers.push(+userInput);
     total = total + parseInt(userInput);
 }
 console.log(numbers);
-alert(`Сумма чисел равна ${total}`);
+// alert(`Сумма чисел равна ${total}`);
 
 
 
--- mod-3/js/script.js
+++ mod-3/js/script.js
@@ -1,45 +1,47 @@
-
 let login = prompt('Введите логин');  
+ 
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 
 
-
-function checkLoginValidity(login){
-    if (login.length > 4 && login.length < 16) {
-        return true;
-        
-    }else{
-        return false;
-       }
-}
- checkLoginValidity = checkLoginValidity(login);
-
-function checkIfLoginExists(logins, login){
-   if(logins.includes(login)){
-        return true;
-
+function checkLoginValidity(name){
+   if (name.length > 4 && name.length < 16) {
+       return true
    }else{
        return false;
-   }  
-}
-var checkIfLoginExists = checkIfLoginExists(logins, login);
-function addLogin(logins, login) {  
-    if(checkLoginValidity == true){
-        if(checkIfLoginExists == true){
-            console.log('Такой логин уже используется!');
-            alert('такой пароль уже существует!');
-        }else{
-            logins = logins.push(login);
-            console.log('Логин успешно добавлен!');
-            alert('Логин успешно добавлен!');
-        }
-    }else{
-        console.log('Ошибка! Логин должен быть от 4 до 16 символов');
-        alert('В логине должен быть от 4 до 16 символов');
-    }
-    return logins;
+      }
 }
 
+
+function checkIfLoginExists(arr, name){
+  if(arr.includes(name)){
+       return true;
+  }else{
+      return false;
+  }  
+}
+
+
+function addLogin(arr, name) {  
+   if(checkLoginValidity(name) == true){
+       if(checkIfLoginExists(arr, name)== true){
+           console.log('Такой логин уже используется!');
+           // alert('такой пароль уже существует!');
+       }else{
+           arr = arr.push(name);
+           console.log('Логин успешно добавлен!');
+           // alert('Логин успешно добавлен!');
+       }
+   }else{
+       console.log('Ошибка! Логин должен быть от 4 до 16 символов');
+       // alert('В логине должен быть от 4 до 16 символов');
+   }
+   return arr;
+}
+
+
+
 addLogin(logins, login);
+
 console.log(logins)
-console.log(logins[logins.length-1])
+
+console.log(logins[logins.length-1]);// выводим последний элемент массива
